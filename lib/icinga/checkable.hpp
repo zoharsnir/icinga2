26ff9f1c1a0c6fb0d6b56fbbc443e3abf266d331
Jean Flach <jean-marcel.flach@icinga.com>
2018-12-07 16:33:10 +0100

    Add severity

--- lib/icinga/checkable.hpp
+++ lib/icinga/checkable.hpp
@@ -53,22 +53,6 @@ enum CheckableType
 	CheckableService
 };
 
-/**
- * Severity Flags
- *
- * @ingroup icinga
- */
-enum SeverityFlag
-{
-	SeverityFlagDowntime = 1,
-	SeverityFlagAcknowledgement = 2,
-	SeverityFlagUnhandled = 8,
-	SeverityFlagPending = 16,
-	SeverityFlagWarning = 32,
-	SeverityFlagUnknown = 64,
-	SeverityFlagCritical = 128,
-};
-
 class CheckCommand;
 class EventCommand;
 class Dependency;
