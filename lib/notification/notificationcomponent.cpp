3dccd91e64a9d18cd7fdd00e069e60a9c4b335c9
Noah Hilverling <noah.hilverling@icinga.com>
2020-06-16 16:37:20 +0200

    WIP

--- lib/notification/notificationcomponent.cpp
+++ lib/notification/notificationcomponent.cpp
@@ -63,61 +63,97 @@ void NotificationComponent::Stop(bool runtimeRemoved)
  */
 void NotificationComponent::NotificationTimerHandler()
 {
+	Log(LogCritical, "DEBUG", "lolwtf 1");
+
 	double now = Utility::GetTime();
 
 	/* Function already checks whether 'api' feature is enabled. */
 	Endpoint::Ptr myEndpoint = Endpoint::GetLocalEndpoint();
 
+	Log(LogCritical, "DEBUG", "lolwtf 2");
+
 	for (const Notification::Ptr& notification : ConfigType::GetObjectsByType<Notification>()) {
-		if (!notification->IsActive())
+		Log(LogCritical, "DEBUG", "lolwtf 2.1");
+		if (!notification->IsActive()) {
+			Log(LogCritical, "DEBUG", "lolwtf 2.1.1");
 			continue;
+		}
+
+		Log(LogCritical, "DEBUG", "lolwtf 2.2");
 
 		String notificationName = notification->GetName();
+		Log(LogCritical, "DEBUG", "lolwtf 2.3");
 		bool updatedObjectAuthority = ApiListener::UpdatedObjectAuthority();
+		Log(LogCritical, "DEBUG", "lolwtf 2.4");
 
 		/* Skip notification if paused, in a cluster setup & HA feature is enabled. */
 		if (notification->IsPaused()) {
+			Log(LogCritical, "DEBUG", "lolwtf 2.4.1");
 			if (updatedObjectAuthority) {
+				Log(LogCritical, "DEBUG", "lolwtf 2.4.1.1");
 				auto stashedNotifications (notification->GetStashedNotifications());
+				Log(LogCritical, "DEBUG", "lolwtf 2.4.1.2");
 				ObjectLock olock(stashedNotifications);
 
+				Log(LogCritical, "DEBUG", "lolwtf 2.4.1.3");
 				if (stashedNotifications->GetLength()) {
+					Log(LogCritical, "DEBUG", "lolwtf 2.4.1.3.1");
 					Log(LogNotice, "NotificationComponent")
 						<< "Notification '" << notificationName << "': HA cluster active, this endpoint does not have the authority. Dropping all stashed notifications.";
 
 					stashedNotifications->Clear();
+					Log(LogCritical, "DEBUG", "lolwtf 2.4.1.3.2");
 				}
+				Log(LogCritical, "DEBUG", "lolwtf 2.4.1.4");
 			}
 
+			Log(LogCritical, "DEBUG", "lolwtf 2.4.2");
 			if (myEndpoint && GetEnableHA()) {
+				Log(LogCritical, "DEBUG", "lolwtf 2.4.2.1");
 				Log(LogNotice, "NotificationComponent")
 					<< "Reminder notification '" << notificationName << "': HA cluster active, this endpoint does not have the authority (paused=true). Skipping.";
 				continue;
 			}
 		}
 
+		Log(LogCritical, "DEBUG", "lolwtf 2.5");
+
 		Checkable::Ptr checkable = notification->GetCheckable();
+
+		Log(LogCritical, "DEBUG", "lolwtf 2.6");
 
 		if (!IcingaApplication::GetInstance()->GetEnableNotifications() || !checkable->GetEnableNotifications())
 			continue;
 
+		Log(LogCritical, "DEBUG", "lolwtf 2.7");
+
 		bool reachable = checkable->IsReachable(DependencyNotification);
 
+		Log(LogCritical, "DEBUG", "lolwtf 2.8");
+
 		if (reachable) {
+			Log(LogCritical, "DEBUG", "lolwtf 2.8.1");
 			Array::Ptr unstashedNotifications = new Array();
 
 			{
+				Log(LogCritical, "DEBUG", "lolwtf 2.8.1.1");
 				auto stashedNotifications (notification->GetStashedNotifications());
+				Log(LogCritical, "DEBUG", "lolwtf 2.8.1.2");
 				ObjectLock olock(stashedNotifications);
 
+				Log(LogCritical, "DEBUG", "lolwtf 2.8.1.3");
 				stashedNotifications->CopyTo(unstashedNotifications);
+				Log(LogCritical, "DEBUG", "lolwtf 2.8.1.4");
 				stashedNotifications->Clear();
 			}
 
 			ObjectLock olock(unstashedNotifications);
 
+			Log(LogCritical, "DEBUG", "lolwtf 2.8.2");
 			for (Dictionary::Ptr unstashedNotification : unstashedNotifications) {
+				Log(LogCritical, "DEBUG", "lolwtf 2.8.2.1");
 				try {
+					Log(LogCritical, "DEBUG", "lolwtf 2.8.2.1.1");
 					Log(LogNotice, "NotificationComponent")
 						<< "Attempting to send stashed notification '" << notificationName << "'.";
 
@@ -129,58 +165,85 @@ void NotificationComponent::NotificationTimerHandler()
 						(String)unstashedNotification->Get("author"),
 						(String)unstashedNotification->Get("text")
 					);
+
+					Log(LogCritical, "DEBUG", "lolwtf 2.8.2.1.2");
 				} catch (const std::exception& ex) {
+					Log(LogCritical, "DEBUG", "lolwtf 2.8.2.2");
 					Log(LogWarning, "NotificationComponent")
 						<< "Exception occurred during notification for object '"
 						<< notificationName << "': " << DiagnosticInformation(ex, false);
 				}
+				Log(LogCritical, "DEBUG", "lolwtf 2.8.2.3");
 			}
 		}
 
+		Log(LogCritical, "DEBUG", "lolwtf 2.9");
+
 		if (notification->GetInterval() <= 0 && notification->GetNoMoreNotifications()) {
+			Log(LogCritical, "DEBUG", "lolwtf 2.9.1");
 			Log(LogNotice, "NotificationComponent")
 				<< "Reminder notification '" << notificationName << "': Notification was sent out once and interval=0 disables reminder notifications.";
 			continue;
 		}
 
+		Log(LogCritical, "DEBUG", "lolwtf 2.10");
 		if (notification->GetNextNotification() > now)
 			continue;
 
+
+		Log(LogCritical, "DEBUG", "lolwtf 2.11");
 		{
 			ObjectLock olock(notification);
+			Log(LogCritical, "DEBUG", "lolwtf 2.11.1");
 			notification->SetNextNotification(Utility::GetTime() + notification->GetInterval());
 		}
 
+		Log(LogCritical, "DEBUG", "lolwtf 2.12");
 		{
 			Host::Ptr host;
 			Service::Ptr service;
 			tie(host, service) = GetHostService(checkable);
+			Log(LogCritical, "DEBUG", "lolwtf 2.12.1");
 
 			ObjectLock olock(checkable);
 
+			Log(LogCritical, "DEBUG", "lolwtf 2.12.2");
 			if (checkable->GetStateType() == StateTypeSoft)
 				continue;
 
+			Log(LogCritical, "DEBUG", "lolwtf 2.12.3");
 			/* Don't send reminder notifications for OK/Up states. */
 			if ((service && service->GetState() == ServiceOK) || (!service && host->GetState() == HostUp))
 				continue;
 
+			Log(LogCritical, "DEBUG", "lolwtf 2.12.4");
+
 			/* Skip in runtime filters. */
 			if (!reachable || checkable->IsInDowntime() || checkable->IsAcknowledged() || checkable->IsFlapping())
 				continue;
+
+			Log(LogCritical, "DEBUG", "lolwtf 2.12.5");
 		}
 
+		Log(LogCritical, "DEBUG", "lolwtf 2.13");
 		try {
+			Log(LogCritical, "DEBUG", "lolwtf 2.13.1.1");
 			Log(LogNotice, "NotificationComponent")
 				<< "Attempting to send reminder notification '" << notificationName << "'.";
 
+			Log(LogCritical, "DEBUG", "lolwtf 2.13.1.2");
 			notification->BeginExecuteNotification(NotificationProblem, checkable->GetLastCheckResult(), false, true);
+			Log(LogCritical, "DEBUG", "lolwtf 2.13.1.3");
 		} catch (const std::exception& ex) {
+
+			Log(LogCritical, "DEBUG", "lolwtf 2.13.2.1");
 			Log(LogWarning, "NotificationComponent")
 				<< "Exception occurred during notification for object '"
 				<< notificationName << "': " << DiagnosticInformation(ex, false);
 		}
+		Log(LogCritical, "DEBUG", "lolwtf 2.14");
 	}
+	Log(LogCritical, "DEBUG", "lolwtf 3");
 }
 
 /**
